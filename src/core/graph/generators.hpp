48bda307ce7d07396e36862e7e761783056cfc4e
Allegra Strippoli <allegra@strippoli.eu>
2025-02-24 18:49:16 +0100

    add generators

--- src/core/graph/generators.hpp
+++ src/core/graph/generators.hpp
@@ -46,4 +46,56 @@ SimpleGraph* generate_connected_random_graph_degree_max_4(size_t number_of_nodes
     return graph;
 }
 
+// n*m grid, n, m > 1
+SimpleGraph *generate_grid_graph(size_t n, size_t m)
+{
+    int num_nodes = 2 * n + 2 * m - 4;
+    SimpleGraph *g = new SimpleGraph();
+    for (int i = 0; i < num_nodes; ++i)
+    {
+        g->add_node();
+    }
+    for (int i = 0; i < num_nodes - 1; ++i)
+    {
+        g->add_undirected_edge(i, i + 1);
+    }
+    g->add_undirected_edge(0, num_nodes - 1);
+    for (int i = 1; i < n - 1; ++i)
+    {
+        g->add_undirected_edge(i, (2 * n) + m - i - 3);
+    }
+    m -= 2;
+    for (int i = 0; i < m; ++i)
+    {
+        g->add_undirected_edge(n + i, (2 * n) + (2 * m) - i - 1);
+    }
+    return g;
+}
+
+// num_nodes > 1
+SimpleGraph *generate_triangle_graph(size_t num_nodes)
+{
+    num_nodes = 3 * num_nodes;
+    SimpleGraph *g = new SimpleGraph();
+    for (int i = 0; i < num_nodes; ++i)
+    {
+        g->add_node();
+    }
+
+    for (int i = 0; i < num_nodes - 3; ++i)
+    {
+        if (i % 3 == 2)
+        {
+            g->add_undirected_edge(i, i + 3);
+            g->add_undirected_edge(i + 3, i - 2);
+        }
+        else
+        {
+            g->add_undirected_edge(i, i + 3);
+            g->add_undirected_edge(i + 1, i + 3);
+        }
+    }
+    return g;
+}
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@ int main() {
     SimpleGraph* graph = generate_connected_random_graph_degree_max_4(45, 60);
     // SimpleGraph* graph = generate_connected_random_graph_degree_max_4(70, 110);
     // SimpleGraph* graph = loadSimpleUndirectedGraphFromFile("example-graphs/g8.txt");
+    // SimpleGraph *graph = generate_grid_graph(15, 10);
+    // SimpleGraph *graph = generate_triangle_graph(2);
+
     auto start_inc = std::chrono::high_resolution_clock::now();
     make_rectilinear_drawing_incremental(*graph);
     auto end_inc = std::chrono::high_resolution_clock::now();
