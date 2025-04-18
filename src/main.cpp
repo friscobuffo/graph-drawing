b6cff8f80ec4091a185f64b7c87ede7b21a1d722
friscobuffo <gio.andreola00@gmail.com>
2025-04-18 17:00:27 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,26 +4,19 @@
 #include "orthogonal/drawing_builder.hpp"
 #include "core/graph/file_loader.hpp"
 #include "core/graph/generators.hpp"
+#include "config/config.hpp"
+#include "baseline-ogdf/drawer.hpp"
 
 int main() {
-    const std::string filename = "drawing.svg";
-    int test_number = 0;
-    while (true) {
-        std::cout << "\rtest number: " << ++test_number << std::flush;
-        auto graph = load_simple_undirected_graph_from_txt_file("generated-graphs/32/graph_92_n32_m58.txt");
-        auto result = make_rectilinear_drawing_incremental_disjoint_paths<SimpleGraph>(*graph);
-        node_positions_to_svg(
-            *result.positions,
-            *result.augmented_graph,
-            filename
-        );
-        if (result.number_of_added_cycles > 0) {
-            std::cout << std::endl;
-            graph->print();
-            break;
-        }
-        //create_drawing(*graph, "output.svg");
-        break;
-    }
+    auto config = parse_config("config.txt");
+    const std::string filename = config["output_svg_shape_metrics"];
+    auto graph = load_simple_undirected_graph_from_txt_file(config["input_graph_file"]);
+    auto result = make_rectilinear_drawing_incremental_disjoint_paths<SimpleGraph>(*graph);
+    node_positions_to_svg(
+        *result.positions,
+        *result.augmented_graph,
+        filename
+    );
+    create_drawing(*graph, "output.svg");
     return 0;
 }
